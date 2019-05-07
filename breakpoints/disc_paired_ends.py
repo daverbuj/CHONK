2cf2f52762322dc0dc98304a458c551100349508
Dan Averbuj <dan.averbuj@gmail.com>
2019-05-07 15:23:16 -0700

    Added meta dict and strand orientation
    Added initial loop to add metadata to a dictionary, and also added calling of SVs with strand orientation

--- breakpoints/disc_paired_ends.py
+++ breakpoints/disc_paired_ends.py
@@ -11,6 +11,22 @@ if meta_directory[-1] != '/':
 	meta_directory = meta_directory + '/'
 chrom = sys.argv[3]
 
+# Metafile to dict
+meta_dict = {}
+for filename_rm in os.listdir(meta_directory):
+	if filename_rm.endswith('.meta.tsv'): 
+		metafile = filename_rm
+		meta_genome = metafile[:7]
+		metafile_abspath = meta_directory + metafile
+		with open(metafile_abspath) as tsv:
+			for meta in csv.reader(tsv, dialect="excel-tab"):
+				meta_chrom = meta[0]
+				meta_med_mpd = meta[3]
+				meta_mad = meta[4]
+				if chrom == meta_chrom:
+					meta_dict[meta_genome] = [meta_med_mpd, meta_mad]
+
+
 for filename_rb in os.listdir(bam_directory):
 	if filename_rb.endswith('.bam'):
 		bamfile = filename_rb
@@ -25,23 +41,15 @@ for filename_rb in os.listdir(bam_directory):
 				and Aln.is_read1 # Each mate/pair once
 				):
 				dist = Aln.template_length
-				for filename_rm in os.listdir(meta_directory):
-					if filename_rm.startswith(genome) and filename_rm.endswith('.meta.tsv'): 
-						metafile = filename_rm
-					metafile_abspath = meta_directory + metafile
-					with open(metafile_abspath) as tsv:
-						for meta in csv.reader(tsv, dialect="excel-tab"):
-							meta_chrom = meta[0]
-							meta_med_mpd = meta[3]
-							meta_mad = meta[4]
-							if chrom == meta_chrom:
-								if dist > (float(meta_med_mpd) + (6 * float(meta_mad))):
-									sv = 'Del'
-									breakpoint_start = Aln.reference_end
-									breakpoint_end = Aln.next_reference_start
-									print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
-								if dist < (float(meta_med_mpd) - (99 * float(meta_mad))):
-									sv = 'Dup'
-									breakpoint_start = Aln.next_reference_start
-									breakpoint_end = Aln.reference_end
-									print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
+				med_mpd = meta_dict[genome][0]
+				mad = meta_dict[genome][1]
+				if ((not Aln.is_reverse and dist > 0) or (Aln.is_reverse and dist < 0)) and dist > (float(med_mpd) + (999 * float(mad))):
+					sv = 'Del'
+					breakpoint_start = Aln.reference_end
+					breakpoint_end = Aln.next_reference_start
+					print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
+				if ((not Aln.is_reverse and dist < 0) or (Aln.is_reverse and dist > 0)) and dist < (float(med_mpd) - (999 * float(mad))):
+					sv = 'Dup'
+					breakpoint_start = Aln.next_reference_start
+					breakpoint_end = Aln.reference_end
+					print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
