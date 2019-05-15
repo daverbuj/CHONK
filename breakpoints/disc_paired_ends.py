139c0b1da479663b2fbcb4a8f6a350d183b2daf9
Dan Averbuj <dan.averbuj@gmail.com>
2019-05-15 14:59:57 -0700

    update to abs(dist) and left/right alignment
    Changed distance to absolute value and added left and right alignment as distinction between deletions and duplications

--- breakpoints/disc_paired_ends.py
+++ breakpoints/disc_paired_ends.py
@@ -38,18 +38,24 @@ for filename_rb in os.listdir(bam_directory):
 				and Aln.next_reference_name == Aln.reference_name # Same chr
 				and  Aln.is_reverse != Aln.mate_is_reverse # Opposite strands
 				and not Aln.has_tag('SA') # Is not a split read
-				and Aln.is_read1 # Each mate/pair once
+				and not Aln.is_reverse # Each mate/pair once; from positive strand
 				):
-				dist = Aln.template_length
+				dist = abs(Aln.template_length)
 				med_mpd = meta_dict[genome][0]
 				mad = meta_dict[genome][1]
-				if ((not Aln.is_reverse and dist > 0) or (Aln.is_reverse and dist < 0)) and dist > (float(med_mpd) + (999 * float(mad))):
-					sv = 'Del'
-					breakpoint_start = Aln.reference_end
-					breakpoint_end = Aln.next_reference_start
-					print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
-				if ((not Aln.is_reverse and dist < 0) or (Aln.is_reverse and dist > 0)) and dist < (float(med_mpd) - (999 * float(mad))):
-					sv = 'Dup'
-					breakpoint_start = Aln.next_reference_start
-					breakpoint_end = Aln.reference_end
-					print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv,'\t',genome,'\t',Aln.query_name, sep='')
+				if dist > (float(med_mpd) + (3.5 * float(mad))):
+					if Aln.reference_start < Aln.next_reference_start:
+						sv = 'Del'
+						breakpoint_start = Aln.reference_end
+						breakpoint_end = Aln.next_reference_start
+						print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv, sep='')
+					if Aln.reference_start > Aln.next_reference_start:
+						sv = 'Dup'
+						breakpoint_start = Aln.next_reference_start
+						breakpoint_end = Aln.reference_end
+						print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv, sep='')
+#				if dist < (float(med_mpd) - (3.5 * float(mad))):
+#						sv = 'Inv'
+#						breakpoint_start =
+#						breakpoint_end =
+#						print(chrom,'\t',breakpoint_start,'\t',breakpoint_end,'\t',sv, sep='')
